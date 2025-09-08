7715894997cf7a46d6721503288891092c5e6e36
CODECZERO <as8857981@gamil.com>
2025-09-08 07:36:53 +0530

    added query's and db connection

--- server/db/dbQuery.py
+++ server/db/dbQuery.py
@@ -1,3 +1,65 @@
+from utils import loadQueries, handleError
 from .connection import getConnection, releaseConnection
-from utils.errorHandler import handleError
+
+Queries = loadQueries("./queries.sql")
+
+
+def runQuery(query_name, params=None, commit=False, fetchone=False, fetchall=False):
+    """
+        Generic query runner.
+        - query_name: key from QUERIES dict
+        - params: tuple of parameters for the query
+        - commit: if True, commits the transaction
+        - fetchone: return single row
+        - fetchall: return all rows
+    """
+    sql = Queries.get(query_name)
+    
+    if not sql:
+        raise ValueError(f"Query {query_name} not found in queries.sql")
+    
+    conn = getConnection()
+    
+    try:
+        with conn.cursor() as curr:
+            curr.execute(sql, params)
+            if commit:
+                conn.commit()
+            if fetchone:
+                return curr.fetchone()
+            if fetchall:
+                return curr.fetchall()
+    finally:
+        releaseConnection(conn)
+
+
+@handleError("Fail to create table", internal_error=1)
+def createTable(query_name):
+    """create user table"""
+    return runQuery(query_name, commit=True)
+
+
+
+@handleError("Fail to fecth userData", internal_error=1)
+def getUser(userName):
+    return runQuery("select_user_by_username", params=(userName,), fetchone=True)
+
+
+@handleError("Fail to insert userData", internal_error=1)
+def insertUser(userName, email, passwordHash):
+    return runQuery("insert_user", params=(userName, email, passwordHash),
+                    commit=True, fetchone=True)
+
+
+
+@handleError("Fail to fecth all userData", internal_error=1)
+def getAllUsers():
+    return runQuery("get_all_user", fetchone=True)
+
+
+@handleError("Fail to fecth user by email", internal_error=1)
+def getUserEmail():
+    return runQuery("select_user_by_email", fetchone=True)
+
+
 
--- server/db/queries.sql
+++ server/db/queries.sql
@@ -9,4 +9,19 @@ CREATE TABLE IF NOT EXISTS users{
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 };
 
---
+-- select_user_by_username
+SELECT userId,userName,email,passwordHash,createdAt FROM users
+WHERE userName=%s 
+
+-- insert_user
+INSERT INTO users(userName,email,passwordHash) 
+VALUES(%s,%s,%s)
+ON CONFLICT (userName, email) DO NOTHING
+RETURNING userId;
+
+-- get_all_user
+SELECT * FROM users ORDER BY createdAt DESC;
+
+-- select_user_by_email
+SELECT userId,userName,email,passwordHash,createdAt FROM users
+WHERE email=%s
